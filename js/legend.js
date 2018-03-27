ba50054361eeda2b614e90efb1e602e700ea295b
Travis1282 <tmail20@gmail.com>
2018-03-27 13:57:46 -0500

    labels are attached

--- js/legend.js
+++ js/legend.js
@@ -1,6 +1,23 @@
 ////////////////// DOTS ///////////////////
 
-let circle1 = document.createElement('div');   
-document.body.appendChild(circle1);
-circle1.id = 'circle'
-circle1.style = 'hsla('+(100*.4)+', 100%, 50%, 0.5 )';
+let circle = [];
+let label = []
+for (let i = 0; i <= 700; i += 100 ) {
+	console.log(i)
+
+circle[i] = 'c'+i+'';
+let c = circle[i];
+label[i] ='l'+i+'';
+let l = label[i];
+
+console.log(circle[i])
+c = document.createElement('div');   
+l = document.createElement('div');   
+document.body.appendChild(c);
+document.body.appendChild(l);
+c.className = 'circle';
+l.className = 'label';
+c.style.background = 'hsl('+( i * .4 )+', 90%, 50%)';
+l.innerText = '$'+i;
+
+}
